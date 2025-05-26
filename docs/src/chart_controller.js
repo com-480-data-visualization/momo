03e33d994003cf0ddc93a196a6ea5953eb1b27c9
Jingxuan Sun <89790849+PirxTion@users.noreply.github.com>
2025-05-26 22:16:11 +0200

    align charts

--- docs/src/chart_controller.js
+++ docs/src/chart_controller.js
@@ -11,4 +11,20 @@ window.setSelectedCountry = function (country) {
         document.getElementById("country-pie-select").value = "map";
         if (window.updatePieChart) updatePieChart();
     }
+
+    // Update for Gender Pie Chart
+    if (window.selectedCountryPie !== undefined) { // Assuming we reuse selectedCountryPie
+        // If you created a new global variable for gender pie chart country selection, use that instead.
+        // For example: window.selectedCountryGenderPie = country;
+        // And update the corresponding dropdown:
+        // document.getElementById("country-gender-pie-select").value = "map"; 
+        // For now, we are reusing selectedCountryPie and the existing pie chart's dropdown updates
+        // will suffice if we want both pie charts to react to the same country selection from the map.
+        // If separate country selection for the gender pie chart (when map is clicked) is desired,
+        // this logic needs to be more distinct.
+        // For now, let's assume the gender pie chart also uses "country-pie-select" for map updates.
+        // If "country-gender-pie-select" should also be set to "map", that needs to be added.
+        document.getElementById("country-gender-pie-select").value = "map"; // Added this line
+        if (window.updateGenderPieChart) window.updateGenderPieChart();
+    }
     };
